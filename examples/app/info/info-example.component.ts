17657c25cb990577ea651bc47d28178f2c4211f7
Kate Negrienko <katya@negrienko.com>
2017-10-17 13:51:54 +0300

    [Samples Info block] change ssamples

--- examples/app/info/info-example.component.ts
+++ examples/app/info/info-example.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PipLandingInfoBlockItem } from '../pip-webui2-landing';
 
 
 @Component({
@@ -11,7 +12,16 @@ export class InfoExampleComponent {
   public img: string = '/assets/u500.png';
   public title: string = 'Общая картина';
   public text: string = 'Теперь есть возможность увидеть полную картину жизни и работы в одном месте.';
+  public items: PipLandingInfoBlockItem[] = [];
   constructor() {
-   
+    let item: PipLandingInfoBlockItem = new PipLandingInfoBlockItem();
+    item.title = this.title;
+    item.titleClass = 'pip-orange';
+    item.text = this.text;
+    item.imgPath = this.img;
+    this.items.push(item);
+    this.items.push(item);
+    this.items.push(item);
+    this.items.push(item);
   }
 }
