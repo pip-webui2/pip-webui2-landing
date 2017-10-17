d0f95c7f6abafcda45e45044fb01562c8f77498d
Kate Negrienko <katya@negrienko.com>
2017-10-17 13:51:02 +0300

    [Info block] add new model for one item

---
+++ src/info-block/shared/info-block.model.ts
@@ -0,0 +1,8 @@
+export class PipLandingInfoBlockItem {
+    public text: string;
+    public imgPath?: string;
+    public title: string;
+    public titleClass?: string = '';
+    public imgClass?: string = '';
+    public imgPosition: string = 'top';
+}
